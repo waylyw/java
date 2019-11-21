8ab3ab10703eb7827c6005ead19d397640bd5564
liuyw9 <1203317365@qq.com>
2019-11-21 09:53:02 +0800

    update

--- src/IntWay.java
+++ src/IntWay.java
@@ -1,6 +1,9 @@
 public class IntWay {
     public static void main(String[] args) {
         int x = 100;
-        System.out.println(x);
+        int y = 200;
+        int z = x + y;
+        System.out.println(z);
+
     }
 }
